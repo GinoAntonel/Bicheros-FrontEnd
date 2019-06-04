dab8803b0b524d191627985bad2692b13022266b
Gino <ginoantonel.ga@gmail.com>
2019-06-04 10:31:07 -0300

    Qsy hace mucho no commiteo

--- src/components/Animal/Card.vue
+++ src/components/Animal/Card.vue
@@ -2,87 +2,87 @@
   <v-container grid-list-md text-xs-center>
     <v-layout row wrap>
       <v-flex v-for="animal in animals" xs4>
-          <v-card height="400px">
-            <v-flex v-if="bottomNav == 'img'">
-              <v-img :src="`${animal.photo}`" height="400px" contain></v-img>
-            </v-flex>
-            <v-flex v-if="bottomNav == 'delete'">
-              <v-container>
-                <v-btn @click="deleteAnimal(animal.id_animal)" color="error">Eliminar</v-btn>
-              </v-container>
-            </v-flex>
-            <v-flex v-else-if="bottomNav == 'info'" xs12>
-              <v-container>
-                <v-layout row wrap>
-                  <v-flex xs4>
-                    <v-subheader>Nombre:</v-subheader>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-card-text>
-                      {{animal.name}}
-                    </v-card-text>
-                  </v-flex>
-                </v-layout>
+        <v-card height="400px">
+          <v-flex v-if="getBottomnav(animal.id_animal) == 'img'">
+            <v-img :src="`${animal.photo}`" height="400px" contain></v-img>
+          </v-flex>
+          <v-flex v-if="getBottomnav(animal.id_animal) == 'delete'">
+            <v-container>
+              <v-btn @click="deleteAnimal(animal.id_animal)" color="error">Eliminar</v-btn>
+            </v-container>
+          </v-flex>
+          <v-flex v-else-if="getBottomnav(animal.id_animal)  == 'info'" xs12>
+            <v-container>
+              <v-layout row wrap>
+                <v-flex xs4>
+                  <v-subheader>Nombre:</v-subheader>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    {{animal.name}}
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
 
-                <v-divider inset></v-divider>
+              <v-divider inset></v-divider>
 
-                <v-layout row>
-                  <v-flex xs4>
-                    <v-subheader> Raza:</v-subheader>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-card-text>
-                      {{animal.race}}
-                    </v-card-text>
-                  </v-flex>
-                </v-layout>
+              <v-layout row>
+                <v-flex xs4>
+                  <v-subheader> Raza:</v-subheader>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    {{animal.race}}
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
 
-                <v-divider inset></v-divider>
+              <v-divider inset></v-divider>
 
-                <v-layout row>
-                  <v-flex xs4>
-                    <v-subheader>Fecha encontrado:</v-subheader>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-card-text>
-                      {{animal.date_founded}}
-                    </v-card-text>
-                  </v-flex>
-                </v-layout>
+              <v-layout row>
+                <v-flex xs4>
+                  <v-subheader>Fecha encontrado:</v-subheader>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    {{animal.date_founded}}
+                  </v-card-text>
+                </v-flex>
+              </v-layout>
 
-                <v-divider inset></v-divider>
+              <v-divider inset></v-divider>
 
-                <v-layout row>
-                  <v-flex xs4>
-                    <v-subheader>Lugar encontrado:</v-subheader>
-                  </v-flex>
-                  <v-flex xs8>
-                    <v-card-text>
-                      {{animal.place_founded}}
-                    </v-card-text>
-                  </v-flex>
+              <v-layout row>
+                <v-flex xs4>
+                  <v-subheader>Lugar encontrado:</v-subheader>
+                </v-flex>
+                <v-flex xs8>
+                  <v-card-text>
+                    {{animal.place_founded}}
+                  </v-card-text>
+                </v-flex>
 
-                <v-divider inset></v-divider>
-                </v-layout>
-              </v-container>
-            </v-flex>
-            <v-bottom-nav :active.sync="bottomNav" :color="color" :value="true" absolute dark shift>
-              <v-btn dark value="img">
-                <span>Imagen</span>
-                <v-icon>image</v-icon>
-              </v-btn>
+              <v-divider inset></v-divider>
+              </v-layout>
+            </v-container>
+          </v-flex>
+          <v-bottom-nav :active.sync="bottomNav[animal.id_animal-1]" :value="true"  absolute dark shift>
+            <v-btn dark id="btn" value="img">
+              <span>Imagen</span>
+              <v-icon>image</v-icon>
+            </v-btn>
 
-              <v-btn dark value="info">
-                <span>Informacion</span>
-                <v-icon>info</v-icon>
-              </v-btn>
+            <v-btn dark value="info">
+              <span>Informacion</span>
+              <v-icon>info</v-icon>
+            </v-btn>
 
-              <v-btn dark value="delete">
-                <span>Eliminar</span>
-                <v-icon>block</v-icon>
-              </v-btn>
-            </v-bottom-nav>
-          </v-card>
+            <v-btn dark value="delete">
+              <span>Eliminar</span>
+              <v-icon>block</v-icon>
+            </v-btn>
+          </v-bottom-nav> 
+        </v-card>
       </v-flex>
     </v-layout>
   </v-container>
@@ -93,18 +93,12 @@ import { mapState } from 'vuex'
 
 export default {
   data () {
-      return {
-        bottomNav: 'img'
-      }
-    },
+    return {
+      bottomNav: ['img', 'img', 'img', 'img', 'img', 'img', 'img', 'img', 'img', 'img', 'img']
+    }
+  },
   computed: mapState({
     animals: state => state.animal.animals,
-    color () {
-        switch (this.bottomNav) {
-          case 0: return 'blue'
-          case 1: return 'teal'
-        }
-      }
   }),
   mounted() {
     this.getAnimals()
@@ -114,9 +108,12 @@ export default {
       this.$store.dispatch('animal/obtainAnimals')
     },
     deleteAnimal(id) {
-      console.log(id)
       this.$store.dispatch('animal/deleteAnimal', id) // accion que va a ejecutar el store
       this.$router.go()
+    },
+    getBottomnav(id) {
+      console.log(id)
+      return this.bottomNav[id-1]
     }
   }
 }
